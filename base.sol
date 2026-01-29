c0af03b0907b604f64051b9d50faadd33033aa35
Username175647 <ghostghostgho@yandex.ru>
2026-01-29 22:02:28 +0300

    add vars, constructor, vote

--- base.sol
+++ base.sol
@@ -1,6 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract Vote {
+contract Election {
+
+    string[] public electors;
+    uint256 public maxVotes;
+    uint256 public electionTime;
+
+    mapping(address => uint256) public userVotes;
+    mapping(uint256 => uint256) public votesCount;
+
+    constructor(string[] memory _electors, uint256 _maxVotes, uint256 _electionTime) {
+        electors = _electors;
+        maxVotes = _maxVotes;
+        electionTime = _electionTime;
+    }
+    function vote(uint256 _numberOfElector) public {
+        userVotes[msg.sender] = _numberOfElector;
+        votesCount[_numberOfElector] += 1;
+    }
 
 }
