91ea5908b0598a82b1a5bb081e608cd27052efa4
Username175647 <ghostghostgho@yandex.ru>
2026-01-31 14:48:31 +0300

    add require

--- base.sol
+++ base.sol
@@ -6,18 +6,25 @@ contract Election {
     string[] public electors;
     uint256 public maxVotes;
     uint256 public electionTime;
+    address public contractOwner;
 
-    mapping(address => uint256) public userVotes;
+    mapping(address => bool) public userVotes;
     mapping(uint256 => uint256) public votesCount;
 
     constructor(string[] memory _electors, uint256 _maxVotes, uint256 _electionTime) {
         electors = _electors;
         maxVotes = _maxVotes;
         electionTime = _electionTime;
+        contractOwner = msg.sender;
     }
     function vote(uint256 _numberOfElector) public {
-        userVotes[msg.sender] = _numberOfElector;
+        require(userVotes[msg.sender] == false, "Your address can't vote");
+        require(_numberOfElector < electors.length, "Elector does not exist");
+        require(votesCount[_numberOfElector] <= maxVotes - 1, "Max Votes");
+        require(msg.sender != contractOwner, "Contract owner can't vote");
+        userVotes[msg.sender] = true;
         votesCount[_numberOfElector] += 1;
     }
 
 }
+
