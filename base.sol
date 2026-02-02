87ac7488af2e7d58cbf541be646200c779897ce0
Username175647 <ghostghostgho@yandex.ru>
2026-02-02 19:46:13 +0300

    add errors

--- base.sol
+++ base.sol
@@ -5,23 +5,28 @@ contract Election {
 
     string[] public electors;
     uint256 public maxVotes;
-    uint256 public electionTime;
+    uint256 public electionEndTime;
     address public contractOwner;
 
     mapping(address => bool) public userVotes;
     mapping(uint256 => uint256) public votesCount;
+    error YourAddressCantVote();
+    error ElectorDoesNotExist(uint256 _pickedElector, uint256 _totalElectors);
+
+    //["Alex","Kostya","Micke"]
 
     constructor(string[] memory _electors, uint256 _maxVotes, uint256 _electionTime) {
         electors = _electors;
         maxVotes = _maxVotes;
-        electionTime = _electionTime;
+        electionEndTime = block.timestamp + _electionTime;
         contractOwner = msg.sender;
     }
     function vote(uint256 _numberOfElector) public {
-        require(userVotes[msg.sender] == false, "Your address can't vote");
-        require(_numberOfElector < electors.length, "Elector does not exist");
+        require(userVotes[msg.sender] == false, YourAddressCantVote());
+        require(_numberOfElector < electors.length, ElectorDoesNotExist(_numberOfElector, electors.length));
         require(votesCount[_numberOfElector] <= maxVotes - 1, "Max Votes");
         require(msg.sender != contractOwner, "Contract owner can't vote");
+        require(block.timestamp < electionEndTime, "Voting is over");
         userVotes[msg.sender] = true;
         votesCount[_numberOfElector] += 1;
     }
